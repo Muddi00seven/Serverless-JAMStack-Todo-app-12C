b6de833f0ac3e5c117927ed270b19f7f5dccf25b
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2020-11-09 16:54:20 +0500

    Initial commit

---
+++ functions/todoList/todoList.js
@@ -0,0 +1,100 @@
+// not meant to be run inside the graqhql-gateway function
+// but just shows a copy-pastable example sibling function
+// that would work with graphql-gateway
+const { ApolloServer, gql } = require("apollo-server-lambda");
+const faunadb = require("faunadb"),
+  q = faunadb.query;
+
+const typeDefs = gql`
+  type Query {
+    allTasks: [Task]
+  }
+  type Mutation {
+    addTask(text: String!): Task
+    updateTask(id: ID!): Task
+    deleteTask(id: ID!): Task
+  }
+  type Task {
+    id: ID!
+    text: String!
+    completed: Boolean!
+  }
+`;
+
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_ADMIN_SECRET,
+
+
+});
+const resolvers = {
+  Query: {
+    allTasks: async (root, args, context) => {
+      try {
+        // const result = await client.query(
+        //   q.Map(
+        //     q.Paginate(q.Match(q.Index("all_tasks"))),
+        //     q.Lambda((x) => q.Get(x))
+        //   )
+        // );
+        let result = await client.query(
+          q.Map(
+            q.Paginate(q.Documents(q.Collection("tasks"))),
+            q.Lambda("X", q.Get(q.Var("X")))
+          )
+    
+        )
+        const data = result.data.map((d) => {
+          return {
+            id: d.ref.id,
+            completed: d.data.completed,
+            text: d.data.text,
+          };
+        });
+
+        return data;
+      } catch (error) {
+        console.log(error);
+        return error.toString();
+      }
+    },
+  },
+  Mutation: {
+    addTask: async (_, { text }) => {
+      try {
+        const result = await client.query(
+          q.Create(q.Collection("tasks"), {
+            data: {
+              text,
+              completed: false,
+            },
+          })
+        );
+        return result.data;
+      } catch (error) {
+        return error.toString();
+      }
+    },
+    deleteTask: async (_, { id }) => {
+      try {
+        const reqId = JSON.stringify(id);
+        const reqId2 = JSON.parse(id);
+        console.log(id);
+
+        const result = await client.query(
+          q.Delete(q.Ref(q.Collection("tasks"), id))
+        );
+        console.log(result);
+        return result.data;
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+exports.handler = server.createHandler();
---
+++ src/pages/index.tsx
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import gql from "graphql-tag";
+import "../css/main.css";
+import {
+  Button,
+  TextField,
+  makeStyles,
+  Container,
+  Box,
+  Typography,
+  Grid,
+  IconButton,
+  CircularProgress,
+} from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+
+const MyStyle = makeStyles(() => ({
+  mainContainer: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+
+  formContainer: {
+    background: "#f3f3f3",
+    width: "100%",
+    maxWidth: "800px",
+    borderRadius: "15px",
+    border: 'solid',
+    borderColor: 'black',
+  },
+  contentWrapper: {
+    width: "100%",
+    maxWidth: "600px",
+    marginTop: "5%",
+  },
+  Datalist: {
+    background: "#f9f9f9",
+    padding: "10px 20px",
+    marginBottom: "4px",
+  },
+  loadingWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "10px",
+    height: "100px",
+  },
+}));
+
+const getTodos = gql`
+  {
+    allTasks {
+      id
+      text
+      completed
+    }
+  }
+`;
+
+const addTodo = gql`
+  mutation addTask($text: String!) {
+    addTask(text: $text) {
+      text
+    }
+  }
+`;
+
+const deleteTodo = gql`
+  mutation deleteTask($id: ID!) {
+    deleteTask(id: $id) {
+      text
+    }
+  }
+`;
+
+export default function Home() {
+  const classes = MyStyle();
+  const [task, setTask] = useState<string>("");
+
+  const { loading, error, data } = useQuery(getTodos);
+  const [deleteTask] = useMutation(deleteTodo);
+  const [addTask] = useMutation(addTodo);
+  //   if (loading) {
+  //     return <h2>Loading...</h2>;
+  //   }
+
+  if (error) {
+    return <h2>Error</h2>;
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task !== "") {
+      addTask({
+        variables: {
+          text: task,
+        },
+        refetchQueries: [{ query: getTodos }],
+      });
+      setTask("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    console.log(JSON.stringify(id));
+    deleteTask({
+      variables: {
+        id: id,
+      },
+      refetchQueries: [{ query: getTodos }],
+    });
+  };
+
+  return (
+    <Container>
+      <div className={classes.mainContainer}>
+        <Box py={8}>
+          <Typography variant="h5">SERVERLESS GRAPHQL TODO APP</Typography>
+        </Box>
+        <div className={classes.formContainer}>
+          <Box p={4}>
+            <form onSubmit={handleSubmit}>
+              <Box pb={2}>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  value={task}
+                  onChange={(e) => setTask(e.target.value)}
+                  label="Task"
+                  name="task"
+                  required
+                />
+              </Box>
+              <Button type="submit" variant="contained" color="primary">
+                add task
+              </Button>
+            </form>
+          </Box>
+        </div>
+        <div className={classes.contentWrapper}>
+          <Box py={1}>
+            {loading ? (
+              <div className={classes.loadingWrapper}>
+                <CircularProgress />
+              </div>
+            ) : (
+              data.allTasks.map((task) => (
+                <div key={task.id} className={classes.Datalist}>
+                  <Grid container>
+                    <Grid item xs={10} container alignItems="center">
+                      <Typography>{task.text}</Typography>
+                    </Grid>
+                    <Grid container justify="flex-end" item xs={2}>
+                      <IconButton onClick={() => handleDelete(task.id)}>
+                        <CloseIcon color="secondary" fontSize="small" />
+                      </IconButton>
+                    </Grid>
+                  </Grid>
+                </div>
+              ))
+            )}
+          </Box>
+        </div>
+      </div>
+    </Container>
+  );
+}
